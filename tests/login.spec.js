7223d8db330c2a42c56ad77e8a458ec6b3c5af6c
prabhashshahi1 <prabhash.shahi07@gmail.com>
2025-12-28 22:37:01 +0545

    invalid login update

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,11 +2,18 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login';
 
 
-test('test', async ({ page }) => {
+test('test-valid', async ({ page }) => {
   const Login = new LoginPage(page);
-  
+
   await Login.gotoLoginPage(); 
   await Login.login('tomsmith', 'SuperSecretPassword!');
+});
+
+test('test-invalid', async ({ page }) => {
+  const Login = new LoginPage(page);
+
+  await Login.gotoLoginPage(); 
+  await Login.login('tomsmithno', 'noooSuperSecretPassword!');
 });
 
 
