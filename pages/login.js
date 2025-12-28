d8a902d1149324a5a5d151e95ab176e30ee20505
prabhashshahi1 <prabhash.shahi07@gmail.com>
2025-12-28 22:21:41 +0545

    first code push

---
+++ pages/login.js
@@ -0,0 +1,22 @@
+exports.LoginPage = class LoginPage {
+
+    constructor(page) {
+        this.page = page;
+        this.username_textbox = page.getByRole('textbox', { name: 'Username' });
+        this.password_textbox = page.getByRole('textbox', { name: 'Password' });
+        this.login_button = page.getByRole('button', { name: ' Login' });
+    }
+
+
+    //Creating one function for Login unlike the one above  
+    async login(username, password) {
+        await this.username_textbox.fill(username);
+        await this.password_textbox.fill(password);
+        await this.login_button.click();
+    }
+
+    async gotoLoginPage() {
+        await this.page.goto('https://the-internet.herokuapp.com/login');
+    } 
+
+}
